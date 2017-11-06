523f81497fddc1b31c4a9f64457660a612b83291
GroovyChuk <alasdair333@hotmail.com>
2017-11-07 00:19:24 +0100

    SmartSelector now with weighted probability - not working

--- GA/Selectors/SmartSelector.cs
+++ GA/Selectors/SmartSelector.cs
@@ -7,21 +7,53 @@ public class SmartSelector : ISelector
 {
 	public List<string> SelectFromGeneration(GenerationDB.Generation parentGeneration)
 	{
-		List<string> newGeneration = new List<string>();
-		Individual previous = parentGeneration.Fittest;
+		List<Individual> potentialIndividuals = parentGeneration.Individuals; // all individuals
+		List<Individual> selectedIndividuals = new List<Individual>(); // individuals chosen according to fitness	
+		List<string> newGeneration = new List<string>(); // Gene Sequence of choosen individuals in order of recombination	
 
-		foreach (Individual ind in parentGeneration.Individuals)
-		{
-			//Fittest Individual gets to mate two times with 2nd and 3rd fittest
-			if (Individual.Equals(ind,previous)) {
-				newGeneration.Add(ind.GeneSequence);
-				newGeneration.Add(parentGeneration.Individuals[2].GeneSequence);
-			} else{
-				newGeneration.Add(previous.GeneSequence);
-				newGeneration.Add(ind.GeneSequence);
-				previous = ind;
-			}
+		float fitnessSum = .0f;
+		int timesAdded = 0;
+		float weightedProbability = .0f;
+
+		for (int i = 0; i < potentialIndividuals.Count; i++) {
+			fitnessSum += potentialIndividuals[i].Fitness;
 		}
+
+		for (int i = 0; i < potentialIndividuals.Count; i++) {
+			if (fitnessSum > 0) {
+				weightedProbability =  potentialIndividuals[i].Fitness / fitnessSum;
+			} else {
+				weightedProbability =  1 - (potentialIndividuals[i].Fitness / fitnessSum);
+			} 
+
+			// can't be more than 100
+			if (weightedProbability > 1)
+				weightedProbability = 1;
+
+			timesAdded = (int) Math.Floor (weightedProbability * potentialIndividuals.Count * 2);
+
+			for (int j = 0; j < timesAdded; j++) {
+				selectedIndividuals.Add (potentialIndividuals [i]);
+				//Debug.Log (potentialIndividuals [i].Fitness);
+			}
+
+		}
+		//Debug.Log ("fill up");
+		// Fill up free spots with fittest individual 
+		for (int i = selectedIndividuals.Count; i < potentialIndividuals.Count * 2; i++) {
+			Debug.Log (parentGeneration.Fittest.Fitness);
+			selectedIndividuals.Add (parentGeneration.Fittest);
+		}
+		//Debug.Log ("--------------------------------");
+		//Debug.Log ("Count: " + selectedIndividuals.Count);
+		AssemblyCSharp.MyFunctions.Shuffle (selectedIndividuals);
+
+		for (int i = 0; i < selectedIndividuals.Count; i++) {
+			Debug.Log (selectedIndividuals [i].Fitness);
+			newGeneration.Add (selectedIndividuals [i].GeneSequence);
+		}
+			
 		return newGeneration;
 	}
+		
 }
